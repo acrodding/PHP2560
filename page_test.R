22c4a9230523d4fb9d2f0da2a9955f8c94bfb44f
ZhejiaDong <71252555+ZhejiaDong1999@users.noreply.github.com>
2021-12-03 17:23:06 -0500

    Add files via upload

--- page_test.R
+++ page_test.R
@@ -3,6 +3,7 @@ library(tidyverse)
 library(igraph)
 
 adjacencyMatrix<-function(df, sur = "all"){
+  # sur is a defult for surface 
   pl.id1 <- df%>% distinct(winner_id) %>% rename(id = winner_id)
   pl.id2 <- df%>% distinct(loser_id) %>% rename(id = loser_id)
   pl.id <- full_join(pl.id1,pl.id2)
@@ -26,25 +27,41 @@ adjacencyMatrix<-function(df, sur = "all"){
   return(as.matrix(adjM))
 
 }
-G <- adjacencyMatrix(df)
+
+adjM <- adjacencyMatrix(df)
 
 dProbabilityMatrix<-function(G,d=0.85){
+  # 1 step trans probability matrix, d is 1-q is paper
   cs <- colSums(G)
   cs[cs==0] <- 1
   n <- nrow(G)
   delta <- (1-d)/n
   A <- matrix(delta,nrow(G),ncol(G))
+  rownames(A) <- rownames(G)
+  colnames(A) <- colnames(G)
   for (i in 1:n) A[i,] <- A[i,] + d*G[i,]/cs
   A
 }
 
-W <- dProbabilityMatrix(G)
+W <- dProbabilityMatrix(adjM)
 
-calcEigenMatrix<-function(G){
-  x <- Re(eigen(G)$vectors[,1])
-  x/sum(x)
+calcEigenMatrix<-function(W){
+  x <- Re(eigen(W)$vectors[,1])
+  ei<-x/sum(x)
+  ei<-as.data.frame(ei)
+  ei$players.id <- rownames(W)
+  return(ei)
 }
 
-calcEigenMatrix(W)
 
-sort(calcEigenMatrix(W),decreasing = TRUE)
+prescore <- calcEigenMatrix(W) %>% arrange(desc(ei)) %>% rename("preScroe" = ei)
+
+winner <- df %>% select(winner_id,winner_name) %>% rename("players.id" = winner_id, 
+                                                          "players.name" = winner_name)
+loser <- df %>% select(loser_id, loser_name) %>% rename("players.id" = loser_id, 
+                                                        "players.name" = loser_name)
+players <- unique(rbind(winner,loser)) 
+players$players.id <-as.character(players$players.id)
+
+
+PresRusults <- left_join(prescore,players, by = "players.id" )
